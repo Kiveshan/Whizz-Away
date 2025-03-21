e2e0b2112b31daaf080a91019a6b826aa70a5bca
Ntokozo Ngcobo <49594805+NTOKOZOTK@users.noreply.github.com>
2025-03-21 06:44:07 +0200

    Initial Commit
    Frontend pages

---
+++ src/pages/ControllerTrackInstruction.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../css/trackinginstruction.css";
+
+const ControllerTrackInstruction = ({ setCurrentPage }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="tracking-wrapper">
+      <div className="content">
+        {/* Back Button */}
+        <button className="back-button" onClick={() => navigate(-1)}>
+         Back
+        </button>
+
+        <div className="filter-section">
+          <div className="filter-group">
+            <button className="filter-button active">Import</button>
+            <button className="filter-button">Export</button>
+            <button className="filter-button outline">All</button>
+          </div>
+          <div className="filter-group">
+            <button className="filter-button outline">All</button>
+            <button className="filter-button active">In-Progress</button>
+            <button className="filter-button">Complete</button>
+          </div>
+        </div>
+
+        {/* <div className="pagination">
+          <button className="pagination-arrow">←</button>
+          <span className="pagination-text">1 of 10</span>
+          <button className="pagination-arrow">→</button>
+        </div> */}
+
+        <div className="tracking-table-wrapper">
+          <table className="tracking-table">
+            <thead>
+              <tr>
+                <th>Instruction No</th>
+                <th>Type</th>
+                <th>Status</th>
+                <th>File No</th>
+                <th>Assignment</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Instruction 1</td>
+                <td>Import</td>
+                <td>In-Progress</td>
+                <td>77002</td>
+                <td>
+                  <button 
+                    className="view-buttons" 
+                    onClick={() => navigate("/ControllerViewAssignment")}
+                  >
+                    View
+                  </button>
+                </td>
+              </tr>
+              <tr className="even-row">
+                <td>Instruction 2</td>
+                <td>Export</td>
+                <td>In-Progress</td>
+                <td>10014</td>
+                <td>
+                  <button 
+                    className="view-buttons" 
+                    onClick={() => navigate("/ControllerViewAssignment")}
+                  >
+                    View
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>Instruction 3</td>
+                <td>Import</td>
+                <td>Complete</td>
+                <td>93301</td>
+                <td>
+                  <button 
+                    className="view-buttons" 
+                    onClick={() => navigate("/ControllerViewAssignment")}
+                  >
+                    View
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ControllerTrackInstruction;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import '../css/register.css';
+
+const Register = ({switchToLogin}) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    // Implement registration logic here
+    if (password === confirmPassword) {
+      console.log("Registering", { firstName, lastName, companyName, email, password });
+    } else {
+      alert("Passwords do not match");
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <div className="register-form">
+        <center>
+          <div className="logo-container">
+            <img
+              src="/images/whizz-away.png"
+              alt="LOGITECH FLOW Logo"
+              className="logo-image1"
+            />
+          </div>
+        </center>
+        <h1 className="register-title">Register</h1>
+
+        <form onSubmit={handleRegister}>
+          <div className="form-row">
+            <div className="form-group">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                id="firstName"
+                placeholder="Input First Name"
+                className="form-input"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                id="lastName"
+                placeholder="Input Surname"
+                className="form-input"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-group">
+              <label htmlFor="companyName">Company Name</label>
+              <input
+                type="text"
+                id="companyName"
+                placeholder="Input Name"
+                className="form-input"
+                value={companyName}
+                onChange={(e) => setCompanyName(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Input Email"
+                className="form-input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Input Password"
+                className="form-input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                placeholder="Confirm Password"
+                className="form-input"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="login-link">
+        <button onClick={switchToLogin} className="link-button">
+          Already have a profile? Login here
+        </button>
+      </div>
+          <button type="submit" className="submit-button1">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
