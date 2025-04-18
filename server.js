aa722435559e273461bd87e2c123efdd961d5b02
Kiveshan Naidoo <102474568+Kiveshan@users.noreply.github.com>
2025-04-18 14:02:14 +0200

    Removed duplication

--- server.js
+++ server.js
@@ -1724,10 +1724,7 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use((req, res, next) => {
-  console.log(`Unhandled request: ${req.method} ${req.url}`)
-  next()
-})
+
 
 app.get("/api/admin/company-list", verifyToken, async (req, res) => {
   let client
