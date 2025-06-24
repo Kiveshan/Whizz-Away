aaa67dd4c75e14b4368103435771ef5de24b2fce
Kiveshan Naidoo <102474568+Kiveshan@users.noreply.github.com>
2025-06-24 07:55:48 +0200

    Financial doc got their own css

--- client/src/pages/financial_documents/views/DirectorFinancialDocumentsView.jsx
+++ client/src/pages/financial_documents/views/DirectorFinancialDocumentsView.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../../api"; // Import the configured Axios instance
-import "../../instructions/css/ViewClientInstruction.css";
+import "../css/FinancialDocView.css";
 
 const DirectorFinancialDocumentsView = () => {
   const navigate = useNavigate();
@@ -39,7 +39,7 @@ const DirectorFinancialDocumentsView = () => {
   }, []);
 
   return (
-    <div className="">
+    <div className="director-financial-docs-wrapper">
       {/* Back Button */}
       <div className="client-payments-header">
         <button
