de42643088f3d69d96d27fa35bfa34d4505821f7
Kiveshan Naidoo <102474568+Kiveshan@users.noreply.github.com>
2025-06-24 11:07:10 +0200

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -9,11 +9,12 @@ const { Pool } = pkg;
 
 // Create a PostgreSQL connection pool
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: Number.parseInt(process.env.POSTGRES_PORT),
+  user: process.env.RDS_USERNAME || "postgres",
+  host: process.env.RDS_HOSTNAME || "localhost",
+  database: process.env.RDS_DB_NAME || "Whizz-Away-Presentation",
+  password: process.env.RDS_PASSWORD || "123456",
+  port: process.env.RDS_PORT || 5432,
+  ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
   connectionTimeoutMillis: 5000,
   idleTimeoutMillis: 30000,
   max: 20,
